1946a515dbb14a092c0f917afece5c1cbc0a624e
Leonardo de Pinho <pinholeonardo1777@gmail.com>
2025-03-19 10:56:35 -0300

    mergeSort almost done

--- merge.js
+++ merge.js
@@ -1,18 +1,40 @@
 function mergeSort(a){
-    if (a.length <= 1) return a;
+        if (a.length <= 1) return a;
+    
+    
+    
+    //merge function
+    function merge(leftArray, rightArray){
+        let leftPosition = 0;
+        let rightPosition = 0;
+        const mergedArray = [];
+    
+        while(leftPosition < leftArray.length && rightPosition < rightArray.length){
+            if(leftArray[leftPosition] < rightArray[rightPosition]){
+                mergedArray.push(leftArray[leftPosition])
+                leftPosition++
+            } else {
+                mergedArray.push(rightArray[rightPosition])
+                rightPosition++
+            }
+        }
+    
+        return [...mergedArray, ...(leftArray.slice(leftPosition)), ...(rightArray.slice(rightPosition))]
+    }
+
+
+    //divide the array
+    const middleIndex = Math.floor((a.length)/2)
+    const firstHalf = a.slice(0,middleIndex)
+    const secondHalf = a.slice(middleIndex)
+
+
+    return mergeSort(merge(firstHalf, secondHalf))
+    
+    
+    
 }
 
-//check if array already sorted
-    //return array if true
+console.log(mergeSort([2,1,3,5,99]))
+//console.log(merge([4,99], [5, 7, 8]))
 
-    
-//find the smallest number
-    //loop over the array based on its length to find smallest one
-    //remove smallest number from original array
-    //save smallest number on variable
-    //feed reduced array on function once more
-//push removed number into first position of final array
-
-
-
-//returned sorted array
